7b02f717ac9992b6fe7795f18f0e66cca2710249
Anderson-Soares <andsu_ss@hotmail.com>
2020-08-03 13:34:06 -0300

    tentativa de ajuste no backend

--- Desktop/Web_developer/bank/src/main/java/br/com/andcorp/bank/controller/CedulaController.java
+++ Desktop/Web_developer/bank/src/main/java/br/com/andcorp/bank/controller/CedulaController.java
@@ -19,11 +19,11 @@ public class CedulaController {
 	private CedulaDAO dao;
 	
 	@PutMapping("/saque")
-	public ResponseEntity<Integer> updateCedulas(Cedulas notas, int troco200, int cedDuzentos, @RequestBody int valorSaque) {
+	public ResponseEntity<Integer> updateCedulas(Cedulas notas, @RequestBody int valorSaque) {
 		try {
-			cedDuzentos = valorSaque / 200;
-			troco200 = valorSaque % 200; 
+			final int cedDuzentos = valorSaque / 200;
 			notas.setQuantidadeDuzentos(notas.getQuantidadeDuzentos() - cedDuzentos);
+			
 			return ResponseEntity.ok(valorSaque);
 		} 
 		catch(Exception e) {
@@ -31,4 +31,38 @@ public class CedulaController {
 			return ResponseEntity.status(400).build();
 		}		
 	}
+	/*
+	@PutMapping("/saque")
+	public ResponseEntity<Integer> updateCedulas(Cedulas notas, int troco200, int troco100,
+			int troco50, int troco20, int troco10, int troco5, int troco2, int troco1,
+			int cedDuzentos, int cedCem, int cedCinquenta, int cedVinte, int cedDez,
+			int cedCinco, int cedDois, int cedUm, @RequestBody int valorSaque) {
+		try {
+			final int troco200;
+			cedDuzentos = valorSaque / 200;
+			troco200 = valorSaque % 200; 
+			notas.setQuantidadeDuzentos(notas.getQuantidadeDuzentos() - cedDuzentos);
+			
+			final int troco200;
+			
+			cedCem = troco200 / 100;
+			troco100 = troco200 % 100;
+			notas.setQuantidadeCem(notas.getQuantidadeCem() - cedCem);
+			
+			cedCinquenta = troco100 / 50;
+			troco100 = troco100 % 50;
+			notas.setQuantidadeCinquenta(notas.ge - cedCem);
+			
+			cedCem = troco200 / 100;
+			troco100 = troco200 % 100;
+			notas.setQuantidadeCem(notas.getQuantidadeCem() - cedCem);
+			
+			return ResponseEntity.ok(valorSaque);
+		} 
+		catch(Exception e) {
+			// bad request
+			return ResponseEntity.status(400).build();
+		}		
+	}
+	*/
 }
